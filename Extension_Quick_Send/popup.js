5cc9714b214421bf4c7a57198f2c4ff0f388bc27
Nikita Dagar <nikitadagar@gmail.com>
2015-03-22 14:32:11 -0400

    Gets current URL and adds a link in HTML

--- Extension_Quick_Send/popup.js
+++ Extension_Quick_Send/popup.js
@@ -15,7 +15,7 @@ if (localStorage.accessToken) {
 			});
 
 	var friendsUrl = "https://graph.facebook.com/me/friends?" + localStorage.accessToken;
-	console.log(friendsUrl);
+	console.log(graphUrl);
 	var friends_request = $.ajax({
 	  url: friendsUrl,
 	  type: "GET",
@@ -45,4 +45,26 @@ function registerDB(fbid)
 		});
 	});
 }
+
+// code below gets the current tab's URL and inserts it into the HTML
+//currently redirecting to an image. Have to change
+
+var str1 = "<a href=\"http://www.facebook.com/dialog/send?app_id=1387556274895733&link="	//first half of the final message string
+var str2 = "&redirect_uri=http://www.freeemailtutorials.com/freeFacebookTutorials/i/Email%20sent%20confirmation%20message%20in%20your%20Facebook%20account.jpg&display=popup\" target=\"_blank\"> Message </a>"	//second half of the final message string
+
+var tabUrl; //url of the current tab
+var msgStr; //construct the message string for the redirect URL
+
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    //get URL of current tab
+    tabUrl = tabs[0].url;
+    
+    //construct final string
+    msgStr = str1 + tabUrl + str2;
+
+    //add URL to HTML 
+    $("#msg_url").html(msgStr);
+});
+
+
 
