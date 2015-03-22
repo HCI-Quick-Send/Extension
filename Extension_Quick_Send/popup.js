8e5522d314d1cd7d935bf96aa3535bcd51c59d08
vatsalvora <vatsal1@msn.com>
2015-03-21 21:46:44 -0400

    Leftover stuff DB reg

--- Extension_Quick_Send/popup.js
+++ Extension_Quick_Send/popup.js
@@ -1,11 +1,43 @@
+//Store fb_id and gcm_id to database
 if (localStorage.accessToken) {
+	var userUrl = "https://graph.facebook.com/me?" + localStorage.accessToken;
+	
+			var user_request = $.ajax({
+			  url: userUrl,
+			  type: "GET",
+			  error: function() {
+				  console.log("Request failed!");
+				},
+			  success:function(msg) {
+					console.log(msg);
+					registerDB(msg.id);
+				}
+			});
+
 	var graphUrl = "https://graph.facebook.com/me/taggable_friends?" + localStorage.accessToken;
 	console.log(graphUrl);
 	var  json = httpGet(graphUrl);
 	var obj = JSON.parse(json);
-	console.log(json);
+	//console.log(json);
 }
-		
+function registerDB(fbid)
+{
+	chrome.storage.local.get('regid', function(result){
+		var keys = 'fb_id='+fbid+'&gcm_id='+result.regid;
+		console.log(keys);
+		var request = $.ajax({
+			url: "http://ec2-54-152-80-39.compute-1.amazonaws.com:8080/api/users",
+			type: "POST",
+			data: keys,
+			error:function() {
+			  console.log("Request failed!");
+			},
+			success:function(msg) {
+				console.log(msg);
+			}
+		});
+	});
+}
 function httpGet(theUrl)
 {
     var xmlHttp = null;
@@ -15,3 +47,4 @@ function httpGet(theUrl)
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
+
